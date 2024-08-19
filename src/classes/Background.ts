5cc30c2714495cdb184b18abb9b68ac8a78be5a8
K3PAA <jkepa00@gmail.com>
2024-08-19 11:44:13 +0200

    Background tiles refactor, bug to fix - background jumps

--- src/classes/Background.ts
+++ src/classes/Background.ts
@@ -2,21 +2,13 @@ import { assets } from '../lib/constants'
 import { Box, Point } from '../lib/types'
 import { randomRGB } from '../lib/utils'
 
-type BackgroundArray = number[][]
-
-type Grid = [
-  [BackgroundArray, BackgroundArray, BackgroundArray],
-  [BackgroundArray, BackgroundArray, BackgroundArray],
-  [BackgroundArray, BackgroundArray, BackgroundArray]
-]
-
 class Background {
   canvas: HTMLCanvasElement
 
   tileSize = 64
   boardDimensions: Point = {
-    x: 24,
-    y: 16,
+    x: 32,
+    y: 32,
   }
 
   gridSize = 3
@@ -24,7 +16,8 @@ class Background {
 
   board: Box
   offset: Point
-  grid = this.generateTestGrid()
+  // grid = this.generateTestGrid()
+  grid = this.generateGrid()
 
   constructor({ canvas }: { canvas: HTMLCanvasElement }) {
     this.canvas = canvas
@@ -43,6 +36,8 @@ class Background {
       x: 0,
       y: 0,
     }
+
+    this.generateBackgroundArray()
   }
 
   generateTestGrid() {
@@ -61,7 +56,7 @@ class Background {
     return grid
   }
 
-  generateGrid(): Grid {
+  generateGrid() {
     const grid = []
 
     for (let i = 0; i < this.gridSize; i++) {
@@ -69,93 +64,22 @@ class Background {
       for (let j = 0; j < this.gridSize; j++) {
         gridRow.push(this.generateBackgroundArray())
       }
-      grid.push(gridRow as [BackgroundArray, BackgroundArray, BackgroundArray])
+      grid.push(gridRow)
     }
 
-    return grid as Grid
-  }
-
-  update() {
-    // left side
-    if (Math.abs(this.offset.x + this.board.position.x) > this.board.width) {
-      // shift top tiles
-      this.grid[0][2] = this.grid[0][1]
-      this.grid[0][1] = this.grid[0][0]
-      this.grid[0][0] = { color: randomRGB() }
-
-      // shift middle tiles
-      this.grid[1][2] = this.grid[1][1]
-      this.grid[1][1] = this.grid[1][0]
-      this.grid[1][0] = { color: randomRGB() }
-
-      // shift bottom tiles
-      this.grid[2][2] = this.grid[2][1]
-      this.grid[2][1] = this.grid[2][0]
-      this.grid[2][0] = { color: randomRGB() }
-
-      this.offset.x = this.board.width + this.offset.x
-    }
-
-    //right side
-    if (this.offset.x - this.board.position.x > this.board.width) {
-      this.grid[0][0] = this.grid[0][1]
-      this.grid[0][1] = this.grid[0][2]
-      this.grid[0][2] = { color: randomRGB() }
-
-      this.grid[1][0] = this.grid[1][1]
-      this.grid[1][1] = this.grid[1][2]
-      this.grid[1][2] = { color: randomRGB() }
-
-      this.grid[2][0] = this.grid[2][1]
-      this.grid[2][1] = this.grid[2][2]
-      this.grid[2][2] = { color: randomRGB() }
-
-      this.offset.x = this.board.width - this.offset.x
-    }
-
-    // top side
-    if (Math.abs(this.offset.y + this.board.position.y) > this.board.height) {
-      this.grid[2][0] = this.grid[1][0]
-      this.grid[1][0] = this.grid[0][0]
-      this.grid[0][0] = { color: randomRGB() }
-
-      this.grid[2][1] = this.grid[1][1]
-      this.grid[1][1] = this.grid[0][1]
-      this.grid[0][1] = { color: randomRGB() }
-
-      this.grid[2][2] = this.grid[1][2]
-      this.grid[1][2] = this.grid[0][2]
-      this.grid[0][2] = { color: randomRGB() }
-
-      this.offset.y = this.board.height + this.offset.y
-    }
-
-    if (this.offset.y - this.board.position.y > this.board.height) {
-      this.grid[0][0] = this.grid[1][0]
-      this.grid[1][0] = this.grid[2][0]
-      this.grid[2][0] = { color: randomRGB() }
-
-      this.grid[0][1] = this.grid[1][1]
-      this.grid[1][1] = this.grid[2][1]
-      this.grid[2][1] = { color: randomRGB() }
-
-      this.grid[0][2] = this.grid[1][2]
-      this.grid[1][2] = this.grid[2][2]
-      this.grid[2][2] = { color: randomRGB() }
-
-      this.offset.y = this.board.height - this.offset.y
-    }
+    return grid
   }
 
   generateBackgroundArray() {
     const backgroundArray: number[][] = []
 
-    for (let i = 0; i < this.boardDimensions.x; i++) {
+    for (let i = 0; i < this.boardDimensions.y; i++) {
       const row: number[] = []
-      for (let j = 0; j < this.boardDimensions.y; j++) {
+      for (let j = 0; j < this.boardDimensions.x; j++) {
         const zeroOrOne = Math.floor(Math.random() * 1.2)
         const tileNumber =
           zeroOrOne === 0 ? 0 : Math.floor(Math.random() * 12) + 1
+
         row.push(tileNumber)
       }
       backgroundArray.push(row)
@@ -164,18 +88,96 @@ class Background {
     return backgroundArray
   }
 
+  update() {
+    if (Math.abs(this.offset.x + this.board.position.x) > this.board.width) {
+      this.shiftGrid('left')
+      this.updateOffset('x', this.board.width + this.offset.x)
+    }
+
+    if (this.offset.x - this.board.position.x > this.board.width) {
+      this.shiftGrid('right')
+      this.updateOffset('x', this.board.width - this.offset.x)
+    }
+
+    if (Math.abs(this.offset.y + this.board.position.y) > this.board.height) {
+      this.shiftGrid('up')
+      this.updateOffset('y', this.board.height + this.offset.y)
+    }
+
+    if (this.offset.y - this.board.position.y > this.board.height) {
+      this.shiftGrid('down')
+      this.updateOffset('y', this.board.height - this.offset.y)
+    }
+  }
+
+  shiftGrid(direction: 'left' | 'right' | 'up' | 'down') {
+    const size = this.gridSize
+    for (let i = 0; i < size; i++) {
+      if (direction === 'left' || direction === 'right') {
+        console.log('shift - left / right')
+        this.shiftRow(i, direction)
+      } else {
+        console.log('shift - up / down')
+        this.shiftColumn(i, direction)
+      }
+    }
+  }
+
+  shiftRow(row: number, direction: 'left' | 'right') {
+    if (direction === 'left') {
+      for (let i = 2; i > 0; i--) {
+        this.grid[row][i] = this.grid[row][i - 1]
+      }
+      this.grid[row][0] = this.generateBackgroundArray()
+    } else {
+      for (let i = 0; i < 2; i++) {
+        this.grid[row][i] = this.grid[row][i + 1]
+      }
+      this.grid[row][2] = this.generateBackgroundArray()
+    }
+  }
+
+  shiftColumn(col: number, direction: 'up' | 'down') {
+    if (direction === 'up') {
+      for (let i = 2; i > 0; i--) {
+        this.grid[i][col] = this.grid[i - 1][col]
+      }
+      this.grid[0][col] = this.generateBackgroundArray()
+    } else {
+      for (let i = 0; i < 2; i++) {
+        this.grid[i][col] = this.grid[i + 1][col]
+      }
+      this.grid[2][col] = this.generateBackgroundArray()
+    }
+  }
+
+  updateOffset(direction: 'x' | 'y', value: number) {
+    this.offset[direction] = value
+  }
+
   draw(c: CanvasRenderingContext2D) {
     c.save()
 
-    for (let i = -1; i < this.gridSize - 1; i++) {
-      for (let j = -1; j < this.gridSize - 1; j++) {
-        c.fillStyle = this.grid[i + 1][j + 1].color
-        c.fillRect(
-          this.board.position.x + this.board.width * j - this.offset.x,
-          this.board.position.y + this.board.height * i - this.offset.y,
-          this.board.width,
-          this.board.height
-        )
+    for (let x = -1; x < this.gridSize - 1; x++) {
+      for (let y = -1; y < this.gridSize - 1; y++) {
+        const backgroundArray = this.grid[x + 1][y + 1]
+
+        for (let i = 0; i < this.boardDimensions.y; i++) {
+          for (let j = 0; j < this.boardDimensions.x; j++) {
+            const tileNumber = backgroundArray[i][j]
+            c.drawImage(
+              this.tilesImage,
+              tileNumber * 16,
+              0,
+              16,
+              16,
+              j * this.tileSize + this.board.width * y - this.offset.x,
+              i * this.tileSize + this.board.height * x - this.offset.y,
+              this.tileSize,
+              this.tileSize
+            )
+          }
+        }
       }
     }
 
